06b543455138bbeb91395ebf39dc08a24e60d76e
root <root@project-dev.pensoft.local>
2022-12-13 14:04:18 +0200

    sync

---
+++ updates/builder_table_update_pensoft_mailing_groups.php
@@ -0,0 +1,29 @@
+<?php namespace Pensoft\Mailing\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMailingGroups extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_mailing_groups', function($table)
+        {
+            $table->string('replace_from')->nullable();
+            $table->string('replace_to')->nullable();
+            $table->string('add_reply_to')->nullable();
+            $table->string('name_append')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_mailing_groups', function($table)
+        {
+            $table->dropColumn('replace_from');
+            $table->dropColumn('replace_to');
+            $table->dropColumn('add_reply_to');
+            $table->dropColumn('name_append');
+        });
+    }
+}
